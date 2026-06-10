0e5203ad10a1b93c16e8014e1bcc1474ef5234df
shuvro-baset <shuvro.aps.75@gmail.com>
2026-06-10 20:02:54 +0600

    chore: add gitignore file.

--- interenationalization/views.py
+++ interenationalization/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.utils.translation import gettext as _
+
+def home(request):
+    message = _("Hello, world!")
+    print(message)
